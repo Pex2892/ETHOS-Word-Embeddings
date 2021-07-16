db9309cd1df96c051c286d2abd38ea365da223eb
Giuseppe <Pex2892@users.noreply.github.com>
2021-07-16 15:51:10 +0200

    Remove old project

--- GRETANet/word_embeddings/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WordEmbeddingsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'word_embeddings'
--- GRETANet/word_embeddings/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='word-embeddings'),
-    path('runNearestWords/', views.run_nearest_words, name='run-nearest-words'),
-    path('runSimilarityWords/', views.run_similarity_words, name='run-similarity-words'),
-    path('runWordAnalogy/', views.run_word_analogy, name='run-word-analogy'),
-]
