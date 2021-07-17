b2367bf3a7a7d967199cab5c638b0827dcb21d00
Giuseppe <Pex2892@users.noreply.github.com>
2021-07-17 11:04:40 +0200

    Add word analogies

--- word_embeddings/views.py
+++ word_embeddings/views.py
@@ -40,22 +40,26 @@ def run_nearest_words(request):
 
     topn = int(request.POST['topn'])
 
-    if positive_words is not None and negative_words is None:
-        results = model_loaded[model_ids_previous.index(int(request.POST['model_id']))].most_similar(positive=positive_words, topn=topn)
-    elif positive_words is None and negative_words is not None:
-        results = model_loaded[model_ids_previous.index(int(request.POST['model_id']))].most_similar(negative=negative_words, topn=topn)
-    elif positive_words is not None and negative_words is not None:
-        results = model_loaded[model_ids_previous.index(int(request.POST['model_id']))].most_similar(positive=positive_words, negative=negative_words, topn=topn)
-    else:
-        return JsonResponse({'type': 'error', 'title': 'Errore', 'mess': 'Lorem ipsum', 'html': ''})
-
-    # BISOGNA GESTIRE CON IL TRY LA NON PRESENZA DI UNA DELLE CHIAVI
-
-    # print(results)
+    try:
+        if positive_words is not None and negative_words is None:
+            results = model_loaded[model_ids_previous.index(int(request.POST['model_id']))].most_similar(
+                positive=positive_words, topn=topn)
+        elif positive_words is None and negative_words is not None:
+            results = model_loaded[model_ids_previous.index(int(request.POST['model_id']))].most_similar(
+                negative=negative_words, topn=topn)
+        elif positive_words is not None and negative_words is not None:
+            results = model_loaded[model_ids_previous.index(int(request.POST['model_id']))].most_similar(
+                positive=positive_words, negative=negative_words, topn=topn)
+        else:
+            return JsonResponse({'type': 'error', 'title': '<b>An error has occurred</b>',
+                                 'message': 'You have not entered any words.<br />Please try again.', 'html': ''})
+    except KeyError as ke:
+        return JsonResponse({'type': 'error', 'title': '<b>An error has occurred</b>',
+                             'message': f'<b>{ke}</b>.<br />Please try again.', 'html': ''})
 
     html_str = '<div class="table-responsive"><table class="table"><thead class=" text-primary">' \
                '<tr><th> Pos. words </th><th> Neg. words </th><th> Results </th>' \
-               '<th class="text-right"> Similarity </th></tr></thead><tbody>'
+               '<th class="text-right"> Score </th></tr></thead><tbody>'
 
     for i in range(0, len(results)):
         html_str += f'<tr><td>{request.POST["positive_words"]}</td><td>{request.POST["negative_words"]}</td>' \
@@ -63,33 +67,86 @@ def run_nearest_words(request):
         # print(request.POST['positive_words'], request.POST['negative_words'], results[i][0], results[i][1])
     html_str += '</tbody></table>'
 
-    return JsonResponse({'type': 'success', 'title': 'Success!', 'mess': 'Lorem ipsum', 'html': html_str})
+    return JsonResponse({'type': 'success', 'title': 'Finished!', 'message': 'Running successfully completed',
+                         'html': html_str})
 
 
 def run_similarity_words(request):
-    model = load_model(int(request.POST['model_id']))
+    global model_ids_previous
+    global model_loaded
+
+    if len(model_ids_previous) == 0 or int(request.POST['model_id']) not in model_ids_previous:
+        model_ids_previous.append(int(request.POST['model_id']))
+        model_loaded.append(load_model(int(request.POST['model_id'])))
 
     word1 = request.POST['word1']
+    if word1 == '':
+        word1 = None
 
     word2 = request.POST['word2']
+    if word2 == '':
+        word2 = None
 
-    results = model.similarity(word1, word2)
+    try:
+        if word1 is not None and word2 is not None:
+            results = model_loaded[model_ids_previous.index(int(request.POST['model_id']))].similarity(word1, word2)
+        else:
+            return JsonResponse({'type': 'error', 'title': '<b>An error has occurred</b>',
+                                 'message': 'You have not entered any words.<br />Please try again.', 'html': ''})
+    except KeyError as ke:
+        return JsonResponse({'type': 'error', 'title': '<b>An error has occurred</b>',
+                             'message': f'<b>{ke}</b>.<br />Please try again.', 'html': ''})
 
     html_str = '<div class="table-responsive"><table class="table"><thead class=" text-primary">' \
                '<tr><th> Word1 </th><th> Word2 </th><th class="text-right"> Similarity </th></tr></thead><tbody>' \
-               f'<tr><td>{word1}</td><td>{word2}</td><td class="text-right">{round(float(results), 4)}</td></tr></tbody></table>'
+               f'<tr><td>{word1}</td><td>{word2}</td><td class="text-right">{round(float(results), 4)}</td>' \
+               f'</tr></tbody></table>'
 
-    return JsonResponse({'type': 'success', 'title': 'Success!', 'mess': 'Lorem ipsum', 'html': html_str})
+    return JsonResponse({'type': 'success', 'title': 'Finished!', 'message': 'Running successfully completed',
+                         'html': html_str})
 
 
 def run_word_analogy(request):
-    model = load_model(int(request.POST['model_id']))
+    global model_ids_previous
+    global model_loaded
+
+    if len(model_ids_previous) == 0 or int(request.POST['model_id']) not in model_ids_previous:
+        model_ids_previous.append(int(request.POST['model_id']))
+        model_loaded.append(load_model(int(request.POST['model_id'])))
 
     word1 = request.POST['word1']
+    if word1 == '':
+        word1 = None
 
     word2 = request.POST['word2']
+    if word2 == '':
+        word2 = None
 
     word3 = request.POST['word3']
+    if word3 == '':
+        word3 = None
 
-    results = model.most_similar(positive=[word3, word2], negative=[word1])
-    print(results)
+    topn = int(request.POST['topn'])
+
+    try:
+        if word1 is not None and word2 is not None and word3 is not None:
+            results = model_loaded[model_ids_previous.index(int(request.POST['model_id']))].most_similar(
+                positive=[word3, word2], negative=[word1], topn=topn)
+        else:
+            return JsonResponse({'type': 'error', 'title': '<b>An error has occurred</b>',
+                                 'message': 'You have not entered any words.<br />Please try again.', 'html': ''})
+    except KeyError as ke:
+        return JsonResponse({'type': 'error', 'title': '<b>An error has occurred</b>',
+                             'message': f'<b>{ke}</b>.<br />Please try again.', 'html': ''})
+
+    html_str = '<div class="table-responsive"><table class="table"><thead class=" text-primary">' \
+               '<tr><th> Word 1 </th><th> Word 2 </th><th> Word 3 </th><th> Results </th>' \
+               '<th class="text-right"> Score </th></tr></thead><tbody>'
+
+    for i in range(0, len(results)):
+        html_str += f'<tr><td>{word1}</td><td>{word2}</td><td>{word3}</td><td>{results[i][0]}</td>' \
+                    f'<td class="text-right">{round(results[i][1], 4)}</td></tr>'
+    html_str += '</tbody></table>'
+
+    return JsonResponse({'type': 'success', 'title': 'Finished!', 'message': 'Running successfully completed',
+                         'html': html_str})
