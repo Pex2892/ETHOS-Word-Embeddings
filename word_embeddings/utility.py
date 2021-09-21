ffa309256dd9a6fbcc26333f430bb79d48e8af26
Giuseppe <Pex2892@users.noreply.github.com>
2021-09-21 17:59:00 +0200

    add normalize function

--- word_embeddings/utility.py
+++ word_embeddings/utility.py
@@ -28,3 +28,21 @@ def load_model(model_id):
             os.path.join(settings.STATIC_DIR_WE, 'models', list_models[model_id][0]), binary=True)
 
     return model
+
+
+def normalize(fulltext, is_lemma: bool = False) -> list:
+    # Tokenization, is_stop, is_punct, is_space, Lemmatization
+    words_base = []
+    words_lemma = []
+    for token in fulltext:
+        if not token.is_stop:
+            if not token.is_punct:
+                if not token.is_space:
+                    if not token.pos_ == 'NUM' and not token.pos_ == 'SYM':
+                        if len(token.text) > 2:
+                            # print(token.text.lower(),  token.lemma_.lower())
+                            words_base.append(token.text.lower())
+                            words_lemma.append(token.lemma_.lower())
+    if is_lemma:
+        return words_lemma
+    return words_base
