c575bf1036264ff9288ac47b0ed019ac8cff7e5b
Giuseppe <Pex2892@users.noreply.github.com>
2021-09-21 17:58:40 +0200

    add url

--- word_embeddings/urls.py
+++ word_embeddings/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('runNearestWords/', views.run_nearest_words, name='run-nearest-words'),
     path('runSimilarityWords/', views.run_similarity_words, name='run-similarity-words'),
     path('runWordAnalogy/', views.run_word_analogy, name='run-word-analogy'),
+    path('runEvalUncertainty/', views.run_eval_uncertainty, name='run-eval-uncertainty'),
 ]
